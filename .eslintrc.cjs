1ca10e649ac8c3673e22961ef2ee16fd47fa72c9
wener <wenermail@gmail.com>
2023-02-05 16:36:36 +0800

    rm more

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  // extends: ['@wener/wode/eslint-config/base.cjs'],
-  extends: [
-    require.resolve('@wener/wode/eslint-config/base.cjs'),
-  ],
-  rules: {},
-};
